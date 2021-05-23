ea1848f90bc9089c152a393254c9afa278e596a9
fengxinming <fxm0016@126.com>
2021-05-23 15:45:32 +0800

    refactor: 💡 代码调整

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # konva-extra
 
+[![npm package](https://nodei.co/npm/konva-extra.png?downloads=true&downloadRank=true&stars=true)](https://www.npmjs.com/package/konva-extra)
+
 > Some extra shapes for Konva framework
+
+[![NPM version](https://img.shields.io/npm/v/konva-extra.svg?style=flat)](https://npmjs.org/package/konva-extra)
+[![NPM Downloads](https://img.shields.io/npm/dm/konva-extra.svg?style=flat)](https://npmjs.org/package/konva-extra)
+[![](https://data.jsdelivr.com/v1/package/npm/konva-extra/badge)](https://www.jsdelivr.com/package/npm/konva-extra)
 
 ## Installation
 
--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,18 @@
 import buble from '@rollup/plugin-buble';
+import nodeResolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 import match from 'rollup-plugin-match';
 import empty from 'rollup-plugin-empty';
 import combine from 'rollup-plugin-combine';
 import copy from 'rollup-plugin-copy';
 import replaceImports from 'rollup-plugin-replace-imports';
+import { terser } from 'rollup-plugin-terser';
+import camelcase from 'camelcase';
+import pkg from './package.json';
+
+const banner = `/* ${pkg.name}.js v${pkg.version} (c) 2021-${new Date().getFullYear()} `
+             + 'Jesse Feng Released under the MIT License. */';
+const globalName = camelcase(pkg.name);
 
 export default [
   {
@@ -12,20 +21,20 @@ export default [
     plugins: [
       empty({
         silent: false,
-        dir: 'dist',
+        dir: 'dist'
       }),
       copy({
         targets: [
           { src: 'package.json', dest: 'dist' },
-          { src: 'TNPM_README.md', dest: 'dist', rename: 'README.md' },
-        ],
+          { src: 'README.md', dest: 'dist', rename: 'README.md' }
+        ]
       }),
       match(),
       combine({
         outputDir: true,
-        camelCase: { pascalCase: true },
+        camelCase: { pascalCase: true }
       }),
-      buble(),
+      buble()
     ],
     output: [
       {
@@ -33,13 +42,66 @@ export default [
         format: 'cjs',
         exports: 'auto',
         plugins: [
-          replaceImports(n => n.replace('/es/', '/')),
-        ],
+          replaceImports((n) => n.replace('/es/', '/'))
+        ]
       },
       {
         dir: 'dist/es',
-        format: 'es',
-      },
+        format: 'es'
+      }
+    ]
+  }, {
+    input: 'src/*.js',
+    external: ['konva'],
+    plugins: [
+      match(),
+      combine({
+        camelCase: false
+      }),
+      nodeResolve(),
+      commonjs(),
+      buble()
     ],
-  },
+    output: [{
+      banner,
+      globals: {
+        konva: 'konva'
+      },
+      file: 'dist/umd.js',
+      name: globalName,
+      format: 'umd',
+      exports: 'auto'
+    }, {
+      banner,
+      globals: {
+        konva: 'konva'
+      },
+      file: 'dist/umd.min.js',
+      name: globalName,
+      format: 'umd',
+      exports: 'auto',
+      plugins: [terser({
+        output: { preamble: banner }
+      })]
+    }, {
+      banner,
+      globals: {
+        konva: 'konva'
+      },
+      file: 'dist/iife.js',
+      name: globalName,
+      format: 'iife'
+    }, {
+      banner,
+      globals: {
+        konva: 'konva'
+      },
+      file: 'dist/iife.min.js',
+      name: globalName,
+      format: 'iife',
+      plugins: [terser({
+        output: { preamble: banner }
+      })]
+    }]
+  }
 ];
